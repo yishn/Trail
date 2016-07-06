9a30f0d3c2c3755719db72505ce262ec3d9b3dee
Yichuan Shen <shen.yichuan@gmail.com>
2016-07-06 21:46:35 +0200

    Remove split.js

--- view/index.js
+++ view/index.js
@@ -5,15 +5,10 @@ const $ = require('../modules/sprint')
 const setting = require('../modules/setting')
 
 function initializeSplitView() {
-    var splitjs = require('split.js')
-    var width = parseFloat($('#root').css('width'))
     var sidebarWidth = parseFloat(setting.get('sidebar.width'))
-
-    splitjs(['#sidebar', 'main'], {
-        sizes: [(sidebarWidth - 1) + 'px', (width - sidebarWidth - 1) + 'px'],
-        gutterSize: 2,
-        snapOffset: 1
-    })
+    $('#sidebar').css('width', sidebarWidth)
+    $('#sidebar + .resizer').css('left', sidebarWidth)
+    $('main').css('left', sidebarWidth + 2)
 }
 
 function initializeSidebar() {
