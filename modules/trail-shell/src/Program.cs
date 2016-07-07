2d56a0dcf53af1e6c371bd5c48cadb9478f1a402
Yichuan Shen <shen.yichuan@gmail.com>
2016-07-07 21:44:28 +0200

    Add IconHelper

--- modules/trail-shell/src/Program.cs
+++ modules/trail-shell/src/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace TrailShell {
     class Program {
         static void Main(string[] args) {
+            while (true) {
+                string input = Console.ReadLine();
+                Console.WriteLine(generateResponse(input) + "\n\n");
+            }
+        }
+
+        private static string generateResponse(string input) {
+            throw new NotImplementedException();
         }
     }
 }
