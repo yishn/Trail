fedab0906c3e4fd180d9935e87a4e86213643688
Yichuan Shen <shen.yichuan@gmail.com>
2016-07-08 23:07:50 +0200

    Handle restart menu item

--- view/ipc.js
+++ view/ipc.js
@@ -1,8 +1,10 @@
 const {ipcRenderer} = require('electron')
+const {app} = require('electron').remote
 
 module.exports = function(Trail) {
     let menudata = {
-        'new-window': () => ipcRenderer.send('new-window')
+        'new-window': () => ipcRenderer.send('new-window'),
+        'restart': () => { app.relaunch(); app.exit(0) }
     }
 
     ipcRenderer.on('menu-click', (e, action) => menudata[action]())
