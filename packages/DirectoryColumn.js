5fb714f641436d36ab3594967591c10147509980
Yichuan Shen <shen.yichuan@gmail.com>
2016-07-09 13:32:02 +0200

    Extract item icons in DirectoryColumn

--- packages/DirectoryColumn.js
+++ packages/DirectoryColumn.js
@@ -1,8 +1,16 @@
-const fs = require('fs-extra')
+const fs = require('fs')
 const {join} = require('path')
 
 const $ = require('../modules/sprint')
+const iconExtractor = require('../modules/icon-extractor')
 const ListColumn = require('../view/ListColumn')
+
+let transformSort = f => (x1, x2) => f(x1) < f(x2) ? -1 : +(f(x1) != f(x2))
+let dirSort = (x1, x2) => {
+    if (x1.folder == x2.folder)
+        return transformSort(x => x.name)(x1, x2)
+    return +x2.folder - +x1.folder
+}
 
 class DirectoryColumn extends ListColumn {
     load(path, callback = () => {}) {
@@ -10,14 +18,37 @@ class DirectoryColumn extends ListColumn {
             if (err) return callback(err)
 
             let items = files.map(name => {
+                let filepath = join(path, name)
+                let folder = false
+                try { folder = fs.lstatSync(filepath).isDirectory() }
+                catch (_) {}
+
                 return {
-                    name
+                    name,
+                    path: filepath,
+                    folder
                 }
-            })
+            }).sort(dirSort)
 
             this.data = {items}
-
             callback(err)
+
+            items.forEach((item, i) => {
+                let updateIcon = (err, img) => {
+                    if (err) {
+                        img = '../node_modules/octicons/build/svg/circle-slash.svg'
+                    }
+
+                    item.icon = img
+                    this.$element.find('li').eq(i).find('img').attr('src', img)
+                }
+
+                if (item.folder) {
+                    iconExtractor.get('folder', true, updateIcon)
+                } else {
+                    iconExtractor.get(item.path, true, updateIcon)
+                }
+            })
         })
     }
 }
