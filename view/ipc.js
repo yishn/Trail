9b08029c94466778768c4aad7590f354fefbafec
Yichuan Shen <shen.yichuan@gmail.com>
2016-07-10 16:57:14 +0200

    Add 'New/Close Tab' menu item

--- view/ipc.js
+++ view/ipc.js
@@ -1,9 +1,13 @@
 const {ipcRenderer} = require('electron')
 const {app} = require('electron').remote
 
+const $ = require('../modules/sprint')
+
 module.exports = function(Trail) {
     let menudata = {
         'new-window': () => ipcRenderer.send('new-window'),
+        'new-tab': () => Trail.TabBar.emit('addbutton-click'),
+        'close-tab': () => Trail.TabBar.closeTab(Trail.TabBar.$element.find('.selected')),
         'restart': () => { app.relaunch(); app.exit(0) }
     }
 
