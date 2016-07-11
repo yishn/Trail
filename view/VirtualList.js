fccc367ebfaabae128beb067aa0d1ffdfae2f588
Yichuan Shen <shen.yichuan@gmail.com>
2016-07-12 00:42:23 +0200

    Finish VirtualList component

--- view/VirtualList.js
+++ view/VirtualList.js
@@ -3,7 +3,9 @@ const Component = require('./Component')
 
 class VirtualList extends Component {
     constructor($element, data) {
-
+        $element.on('scroll', () => {
+            renderChunk()
+        })
     }
 
     render() {
@@ -21,11 +23,28 @@ class VirtualList extends Component {
         let percent = scrollMiddle / scrollHeight
         let shownItemsCount = height / this.data.itemHeight
         let renderItemsCount = shownItemsCount * 4
-        let startIndex = Math.max(0, Math.round(percent * data.items.length) - renderItemsCount / 2)
-        let endIndex = startIndex + renderItemsCount - 1
+        let startIndex = Math.max(0, Math.round(percent * this.data.items.length) - renderItemsCount / 2)
+        let endIndex = Math.min(startIndex + renderItemsCount - 1, this.data.items.length - 1)
+        let $lis = this.$element.children('li')
+        let newItems = []
+
+        $lis.eq(startIndex).prevAll('li').remove()
+        $lis.eq(endIndex).nextAll('li').remove()
 
         for (let i = startIndex; i <= endIndex; i++) {
+            if (this.$element.children(`li[data-index="${i}"]`).length)
+                continue
 
+            let item = this.data.items[i]
+
+            newItems.push(
+                $('<li/>').text(item.name).attr('data-index', i).prepend(
+                    $('<img/>').src(item.icon)
+                )
+            )
         }
+
+        this.$element.append(newItems)
+        this.emit('chunk-rendered')
     }
 }
