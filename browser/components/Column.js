f1ef708066c19ea4b78f1593945aed1be0ea3011
Yichuan Shen <shen.yichuan@gmail.com>
2016-11-18 17:21:57 +0100

    Virtual list for Column

--- browser/components/Column.js
+++ browser/components/Column.js
@@ -6,8 +6,12 @@ class Column extends Component {
     constructor() {
         super()
 
+        this.itemHeight = 25
+
         this.state = {
-            scrollTop: 0,
+            scrollPercentage: 0,
+            height: 0,
+
             items: [],
             icons: [],
             selectedIndices: [],
@@ -19,16 +23,14 @@ class Column extends Component {
         let l = Location.resolve(this.props.location)
 
         l.list((err, items) => {
-            if (err) {
-                items = []
-                this.setState({error: true})
-            }
+            if (err) return this.setState({items: [], error: true})
 
             let selectedIndices = []
             let i = items.findIndex(item => item.path == this.props.initialSelectedPath)
             if (i >= 0) selectedIndices.push(i)
 
             this.setState({items, selectedIndices})
+            this.updateScrollState()
 
             items.forEach((item, i) => {
                 let {icons} = this.state
@@ -43,16 +45,50 @@ class Column extends Component {
         })
     }
 
+    componentDidMount() {
+        let ol = this.element.querySelector('ol')
+        ol.addEventListener('scroll', () => this.updateScrollState())
+        this.updateScrollState()
+    }
+
+    updateScrollState() {
+        let ol = this.element.querySelector('ol')
+        let height = ol.offsetHeight
+        let {scrollTop, scrollHeight} = ol
+
+        this.setState({
+            scrollPercentage: scrollTop / scrollHeight,
+            height
+        })
+    }
+
     render({location, width, minWidth}, {items, icons, selectedIndices, error}) {
-        return h('section', {class: 'column-box', style: {width}}, [
+        let itemsCount = Math.ceil(this.state.height / this.itemHeight)
+        let startIndex = Math.floor(this.state.scrollPercentage * (items.length - 1))
+        let endIndex = startIndex + itemsCount
+
+        return h('section', {
+            class: 'column-box',
+            style: {width},
+            ref: el => this.element = el
+        }, [
             !error
 
-            ? h('ol', {}, items.map((item, i) =>
-                h('li', {class: {selected: selectedIndices.includes(i)}}, [
-                    h('img', {src: icons[i] || 'img/blank.svg'}),
-                    h('span', {title: item.label}, item.label)
-                ])
-            ))
+            ? h('ol', {}, [
+                items.map((item, i) => {
+                    if (i < startIndex || i > endIndex) return
+
+                    return h('li', {
+                        class: {selected: selectedIndices.includes(i)},
+                        style: {top: i * this.itemHeight}
+                    }, [
+                        h('img', {src: icons[i] || 'img/blank.svg'}),
+                        h('span', {title: item.label}, item.label)
+                    ])
+                }),
+
+                h('li', {class: 'placeholder', style: {height: items.length * this.itemHeight}})
+            ])
 
             : h('div', {class: 'error'}),
 
